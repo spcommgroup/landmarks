f4de50750b440835aa5a3ff4fb4000fe41c7a4f4
Jess Kenney <seveneightn9ne@gmail.com>
2013-05-29 13:10:57 -0400

    Cleaned up & removed old code

--- jessk/convertLM.py
+++ jessk/convertLM.py
@@ -20,24 +20,12 @@ def compileRules():
         return i+a
 
     suffix_pairs = itertools.permutations(["","-x","-+"],2)
-    #print("Suffix pairs:"+str(suffix_pairs))
     for line in f:
         items = line.strip("\n").split("/")
         if len(items) == 5: # keeps empty lines and such from being added
-            #print("Adding rule "+line)
             for s_0 in ["","-x","-+"]:
                 for s_1 in ["","-x","-+"]:
-                    #print(spair)
                     rules[items[0]+s_0+"-"+items[1]+s_1] = [ifAppend(items[2],s_0), ifAppend(items[3],s_0), ifAppend(items[4],s_1)]
-                    #if(items[0]=="#" and items[1]=="V"):
-                    #    print("Adding "+items[0]+s_0+"-"+items[1]+s_1+" -> "+ str([ifAppend(items[2],s_0), ifAppend(items[3],s_0), ifAppend(items[4],s_1)]))
-            # rules[items[0]+"-"+items[1]] = items[2:]
-            # rules[items[0]+"-x-"+items[1]] = [ifAppend(items[2],"-x"), ifAppend(items[3],"-x"), items[4]]
-            # rules[items[0]+"-+-"+items[1]] = [ifAppend(items[2],"-+"), ifAppend(items[3],"-+"), items[4]]
-            # rules[items[0]+"-"+items[1]+"-x"] = [items[2], items[3], ifAppend(items[4],"-x")]
-            # rules[items[0]+"-"+items[1]+"-+"] = [items[2], items[3], ifAppend(items[4],"-+")]
-        #else:
-        #    print("Skipped line "+line)
 
     # Dictionary of conversions from Choi LM (e.g. ng-cl) to LM type (e.g. Nc)
     LMtypes = {"V": "V", "#": "#"}
@@ -55,16 +43,6 @@ def compileRules():
         LMtypes_plus[key+"-x"]= LMtypes[key]+"-x"
         LMtypes_plus[key+"-+"]= LMtypes[key]+"-+"
     LMtypes.update(LMtypes_plus)
-    # for key in rules:
-    #     f,t = key.split("-")
-    #     rules_plus[f+"-x-"+t]= []
-    #     rules_plus[key+"-x"]= [a+"-x" for a in rules[key] if a != ""]
-    #     rules_plus[key+"-+"]= [a+"-+" for a in rules[key] if a != ""]
-    # rules.update(rules_plus)
-
-    # for i in rules:
-    #     if i.startswith("#"):
-    #         print(i+"\t"+str(rules[i]))
 
     #Decide which tiers to operate on:
     print("Choose which tiers to convert. Any unselected tiers will remain intact. Selected tiers will be erased and rewritten.")
@@ -89,94 +67,8 @@ def compileRules():
                 break
     else:
         useTiers = default.strip(",").split(",")
-    #print("useTiers = "+str(useTiers))
     return rules, LMtypes, useTiers
 
-
-
-# def process(t,o, rules, LMtypes, useTiers):
-#     not_found = {}
-#     for tier_i in useTiers:
-#         tier_i = int(tier_i)
-#         tier  = t.tiers[tier_i] # input tier
-#         otier = o.tiers[tier_i] # output tier
-#         toAdd = [] # Add new items at the end so they don't interfere with ids
-#         (last_item_i, last_item) = (None, None)
-#         for (item_i, item) in zip(range(len(tier.items)), tier.items):
-#             if item_i == 0 and type(item)==Point: #First item, implicit "#" beforehand
-#                 if not item.mark.strip() in LMtypes:
-#                     if not item.mark.strip() in not_found:
-#                         not_found[item.mark.strip()] = item.time
-#                     #print("Warning: Item not found in conversion table: "+item.mark)
-#                     otier.items[item_i].mark=""
-#                 else:
-#                     if not "#-"+LMtypes[item.mark] in rules:
-#                         logging.warning("Rule not found: "+"#-"+LMtypes[item.mark])
-#                     else:
-#                         rule = rules["#-"+LMtypes[item.mark]]
-#                         #print("Applying rule "+str(rule)+" to # - "+item.mark)
-#                         otier.items[item_i].mark = rule[2]
-
-#             elif(item_i > 0 and type(item)==Point): #starts on second item, so that last_item is defined
-#                 e=0
-#                 #if last_item == None:
-#                 #    print("What?" + item.mark)
-#                 if not last_item.mark.strip() in LMtypes:
-#                     if not last_item.mark.strip() in not_found:
-#                         not_found[last_item.mark.strip()] = last_item.time
-#                     #print("Warning: Item not found in conversion table: "+last_item.mark)
-#                     otier.items[last_item_i].mark=""
-#                     e=1
-#                 if not item.mark.strip() in LMtypes:
-#                     if not item.mark.strip() in not_found:
-#                         not_found[item.mark.strip()] = item.time
-#                     #print("Warning: Item not found in conversion table: "+item.mark)
-#                     otier.items[item_i].mark=""
-#                     e=1
-#                 if e==0: 
-#                     rule = rules[LMtypes[last_item.mark.strip()]+"-"+LMtypes[item.mark.strip()]]
-#                     #print("Applying rule "+str(rule)+" to "+last_item.mark+" - "+item.mark)
-#                     if(otier.items[last_item_i].mark.strip()==""):
-#                         #print("\tChanging empty li to "+rule[0])
-#                         otier.items[last_item_i].mark = rule[0]
-#                     elif(otier.items[last_item_i].mark.strip() != rule[0] and rule[0]!=""):
-#                         #logging.warning("\tchanged "+otier.items[last_item_i].mark+" to "+rule[0] + " (" + str(last_item.time) + ")")
-#                         otier.items[last_item_i].mark = rule[0]
-#                     if rule[1]:
-#                         #print("\tQueuing point to add "+rule[1])
-#                         toAdd.append(Point(str(float(item.time)-.001), rule[1]))
-#                     if otier.items[item_i].mark in ["+g","-g","+b","-b","+s","-s"]:
-#                         logging.warning("\tChanging "+otier.items[item_i].mark+" to "+rule[2])
-#                     otier.items[item_i].mark = rule[2]
-#             (last_item_i, last_item) = (item_i, item)
-
-#         # Apply last rule, X-# at end of tier
-#         if type(last_item)==Point and not last_item.mark.strip() in LMtypes:
-#             if not last_item.mark.strip() in not_found:
-#                 not_found[last_item.mark.strip()] = last_item.time
-#             #print("Warning: Item not found in conversion table: "+last_item.mark)
-#             otier.items[last_item_i].mark=""
-#         elif type(last_item)==Point: 
-#             rule = rules[LMtypes[last_item.mark.strip()]+"-#"]
-#             #print("Applying rule "+str(rule)+" to "+last_item.mark+" - #")
-#             if(o.tiers[tier_i].items[last_item_i].mark.strip()==""):
-#                 #print("\tChanging empty li to "+rule[0])
-#                 otier.items[last_item_i].mark = rule[0]
-#             elif(otier.items[last_item_i].mark.strip() != rule[0] and rule[0]!=""):
-#                 #logging.warning("\tchanged "+otier.items[last_item_i].mark+" to "+rule[0])
-#                 otier.items[last_item_i].mark = rule[0]
-#             if rule[1]:
-#                 #print("\tQueuing point to add "+rule[1])
-#                 toAdd.append(Point(str(float(tier.xmax)-.001), rule[1]))
-            
-#         for a in toAdd:
-#             #print("Adding point "+str(a))
-#             otier.addPoint(a)
-#         otier.removeBlankPoints()
-#     logging.warning("Not found marks:")
-#     for mark,time in sorted(not_found.items(),key=operator.itemgetter(1)):
-#         logging.warning("\t"+mark + " ("+time +")")
-#     return None
 def process(t, rules, LMtypes, useTiers):
     ctiers = []
     output = []
@@ -258,14 +150,9 @@ if __name__=="__main__":
     filepath = os.path.abspath(sys.argv[1])
     pathsplit = os.path.splitext(filepath)
     destpath = pathsplit[0] + ".processed" + pathsplit[1]
-    
-    t = TextGrid(filepath=filepath)
-    #o = TextGrid(filepath=filepath)#output textgrid
-    
     logging.basicConfig(filename='log.txt',level=logging.DEBUG)
-        
-    # rules = userInterface(t)
 
+    t = TextGrid(filepath=filepath)
     rules, LMtypes, useTiers = compileRules();
     print("Processing TextGrid...\n")
     o = process(t,rules, LMtypes, useTiers)
